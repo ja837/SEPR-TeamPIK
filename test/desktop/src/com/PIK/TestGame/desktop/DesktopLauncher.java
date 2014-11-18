5df8a4d4a7163f16a49e78efa676a9888e8f9167
Jamie Adkins <ja837@york.ac.uk>
2014-11-18 16:37:05 +0000

    Test libGDX project

---
+++ test/desktop/src/com/PIK/TestGame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.PIK.TestGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.PIK.TestGame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
