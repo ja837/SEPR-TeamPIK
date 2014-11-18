0e5e427aa3abd5e98806411f975a46dc86cec35d
Jamie Adkins <ja837@york.ac.uk>
2014-11-18 16:54:11 +0000

    Commenting out some stuff, testing GIT tbh.

--- test/core/src/com/PIK/TestGame/MyGdxGame.java
+++ test/core/src/com/PIK/TestGame/MyGdxGame.java
@@ -18,10 +18,10 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		//Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
