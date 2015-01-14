3b6d91b3b8af5417fc56a7bfa758b35505f89b83
scplant <sp1091@york.ac.uk>
2015-01-14 23:50:28 +0000

    Train and Inventory class created

---
+++ Game/core/src/com/teampik/game/Inventory.java
@@ -0,0 +1,28 @@
+package com.teampik.game;
+
+import java.util.ArrayList;
+
+public class Inventory extends Player {
+	//Trains
+	ArrayList<Train> trains = new ArrayList<Train>();
+
+	public int addTrain(Train train){
+		
+		if (trains.size() < 2) { //If there is less than 3 Trains in the inventory add the train
+			trains.add(train);
+			return 1;
+			}
+		
+		else{
+			return 0;	//otherwise return 0
+		}
+		
+	}
+	
+	public void deployTrain(Train train, MapTile zoo){
+		if (true){//INCOMPLETE CHANGE TO CHECK TILE IS ZOOTILE
+			
+		}
+	}
+	//Powerups
+}
---
+++ Game/core/src/com/teampik/game/Train.java
@@ -0,0 +1,5 @@
+package com.teampik.game;
+
+public class Train {
+
+}
