3f2916c0afcb7741639b680892003e3dc13f8832
lmm523 <lmm523@york.ac.uk>
2015-01-15 16:21:43 +0000

    train creation and speed

--- Game/core/src/com/teampik/game/Train.java
+++ Game/core/src/com/teampik/game/Train.java
@@ -1,5 +1,34 @@
 package com.teampik.game;
 
 public class Train {
-
+	
+	public enum trainType {
+		HOVER, BULLET, ELECTRIC, DIESEL, STEAM
+	}
+	
+	public trainType type;
+	public int speed;
+	
+	// Train constructor
+	public Train(trainType tType){
+		this.type = tType;
+		switch (tType) {
+		    case STEAM:
+		        speed = 2;
+		    case DIESEL:
+		        speed = 4;
+		    case ELECTRIC:
+		        speed = 5;
+		    case BULLET:
+		    	speed = 6;
+		    case HOVER:
+		    	speed = 8;
+		}
+	}
+	
+	//change train speed, will add duration (no of turns to change for) later
+	public void setSpeed(int tspeed){
+		speed = tspeed;
+	}
 }
+
