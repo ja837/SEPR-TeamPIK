74c69d36bb63019bc7cccb07676cc9a1138d2699
scplant <sp1091@york.ac.uk>
2015-01-19 07:23:38 +0000

    Junit hello world

--- Game/core/src/com/teampik/game/MapTest.java
+++ Game/core/src/com/teampik/game/MapTest.java
@@ -1,8 +1,21 @@
 package com.teampik.game; 
 
-import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 
-@RunWith(GdxTestRunner.class)
+import org.junit.Test;
+//import org.junit.runner.RunWith;
+
+//@RunWith(GdxTestRunner.class)
 public class MapTest {
-
+	@Test
+	public void Gamemap_helloworld_test(){
+	
+	//setup
+	
+	//execute
+	//MapTile tile = game.map.getTile.mask();
+	//assert
+		
+		assertEquals(0,0);
+	}
 }
